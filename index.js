2baa259ffc4d2e68239e3f4857d2bbf91b35f57b
Anna Hu <anna7109@gmail.com>
2019-06-18 09:43:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 (function() {
   document.addEventListener("DOMContentLoaded", () => {
-    // ここでいろいろごにょごにょする
+    const selectList = document.getElementById('selectList')
+
+    selectList.addEventListener("change", (event) => {
+      console.log(event);
+      window.location.hash='page-intro';
+    })
+
+    let radio = []
+    let panel = []
+    for(let i=0; i<3; i++){
+      radio[i] = $('.state').eq(i);
+      panel[i] = $('.panel').eq(i);
+    }
+
   });
+
+
+
 })();
+
+
+$('.state').change(function () {
+    $(this).parent().find('.state').each(function () {
+        if (this.checked) {
+            $(this).attr('aria-selected', 'true');
+
+        } else {
+            $(this).removeAttr('aria-selected');
+        }
+    });
+});
